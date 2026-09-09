560c86f3f93a0c874c9b5b413e5b74d11d30012a
logan Blankenbeckler2@gmail.com <loganblankenbeckler2@gmail.com>
2026-09-09 14:49:57 -0400

    Add header comment and introduction lines

--- lab0-hello/HelloWorld/Program.cs
+++ lab0-hello/HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name:Logan Blankenbeckler 
+* Course: CSCI 1250, Section 002
+* Assignment: Lab 0, Hello World
+* Date: September 8, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Logan Blankenbeckler.");
+Console.WriteLine("This is my first program at ETSU.");
